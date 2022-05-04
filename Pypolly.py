617309ff1daa4adc594c43dbc0271daedcda09a7
hajaf <hajafatounjie@gmail.com>
2022-05-04 14:48:14 -0400

    add r files

--- Pypolly.py
+++ Pypolly.py
@@ -1,1 +1,32 @@
+# Add our dependencies.
+import csv
+import os
+# Assign a variable to load a file from a path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
 
+# Open the election results and read the file.
+election_data = open(file_to_load, 'r')
+
+# To do: perform analysis.
+
+# Close the file.
+election_data.close()
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+
+    # Print the file object.
+     print(election_data)
+# Using the with statement open the file as a text file.
+with open(file_to_save, "w") as txt_file:
+
+    # Write some data to the file.
+    txt_file.write("Hello World")
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+    file_reader = csv.reader(election_data)
+
+    # Read and print the header row.
+    headers = next(file_reader)
+    print(headers)
